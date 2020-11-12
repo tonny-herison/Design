8ca3b9cb54cb883a334a5f76611117e98f8f6c81
Tonny HERISON <tonny.jacklin.herison@esti.mg>
2020-11-12 20:04:01 +0300

    Timer tab finished

--- Neumorphism/assets/js/horloge.js
+++ Neumorphism/assets/js/horloge.js
@@ -1,3 +1,8 @@
+var playing = false;
+var time = 0;
+var handletimer;
+var num_inter = 1;
+
 $(document).ready(function(){
     let sec = $("div.seconde");
     let min = $("div.minute");
@@ -6,6 +11,7 @@ $(document).ready(function(){
     $("#alarm").show();
 
     let is = 0, im = 0, ih = 0;
+    // Clock
     setInterval(() => {
         let d = new Date();
         let h = d.getHours(), m = d.getMinutes(), s = d.getSeconds();
@@ -54,3 +60,67 @@ $(document).on("click", ".tab-item a", function (e) {
     $(".tab-content").hide();
     $("#" + targ).show();
 })
+
+$(document).on("click", ".play-bar .play-timer", function (e) {
+    if (playing) {
+        $(this).children().eq(0).addClass("lni-play");
+        $(this).children().eq(0).removeClass("lni-pause");
+    } else {
+        $(this).children().eq(0).addClass("lni-pause");
+        $(this).children().eq(0).removeClass("lni-play");
+    }
+
+    playing = !playing;
+
+    // Timer
+    if (playing) {
+        handletimer = setInterval(() => {
+            let rot = time * 360 / 6000;
+            $(".aiguille").css({ "transform" : "rotate(" + rot + "deg)" });
+            let min = Math.floor((time / 6000) % 60);
+            let sec = Math.floor((time / 100) % 60);
+            let ms = time % 100;
+            $(".timer-time").html(format(min) + ":" + format(sec) + "." + format(ms))
+            time+=1;
+        }, 10);
+    }
+    else{
+        clearInterval(handletimer);
+    }
+    
+})
+
+$(document).on("click", ".play-bar .reset-timer", function (e) {
+    time = 0;
+    playing = false;
+    num_inter = 1;
+    
+    $(".play-bar .play-timer").children().eq(0).addClass("lni-play");
+    $(".play-bar .play-timer").children().eq(0).removeClass("lni-pause");
+
+    $(".timer-time").html("00:00.00")
+    if (handletimer) clearInterval(handletimer);
+
+    $(".aiguille").css({ "transform" : "rotate(0deg)" });
+
+    $(".timer-list ul").html("");
+})
+
+$(document).on("click", ".play-bar .save-timer", function (e) {
+    let min = Math.floor((time / 6000) % 60);
+    let sec = Math.floor((time / 100) % 60);
+    let ms = time % 100;
+
+    let content = '<li>' +
+                    '<span class="timer-title">Itervalle ' + num_inter + '</span>' +
+                    '<span class="timer-value">' + format(min) + ':' + format(sec) +'.' + format(ms) + '</span>' +
+                '</li>'
+    $(".timer-list ul").append(content);
+
+    num_inter++;
+})
+
+let format = function (n) {
+    if (n<10) return '0' + n;
+    return n;
+}
